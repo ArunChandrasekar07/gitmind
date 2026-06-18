1d9c843d3fd0f996b6f6d2fa9b50951f9f8b778d
Arun Chandrasekar <arunchandruselvi@gmail.com>
2026-06-18 15:23:19 +0530

    feat: fixed navbar padding

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -445,7 +445,7 @@ export default function LandingPage() {
               maxWidth: "1180px",
               width: "100%",
               margin: "0 auto",
-              padding: "0 24px",
+              padding: "0 2px",
               display: "flex",
               alignItems: "center",
               justifyContent: "space-between",
